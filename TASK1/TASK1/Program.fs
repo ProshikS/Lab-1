f813c5f0250d599c8db79ec942fe210e38616376
ProshikS <srarpulov.06@mail.ru>
2026-02-27 05:00:45 +0500

    I

---
+++ TASK1/TASK1/Program.fs
@@ -0,0 +1,17 @@
+﻿let digits n = //генератор списков
+    [ for i in 1 .. n do 
+        printf "Введите число: "
+        let number = int(System.Console.ReadLine())
+        yield abs(number%10) //возвращаем последнюю цифру введенного числа
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    printf "Введите количество чисел: "
+    let n = int(System.Console.ReadLine())
+    if n > 0 then //проверка на корректность ввода количества
+        let list_of_digits = digits n;
+        printfn "Последние цифры введенных чисел: %A" list_of_digits
+    else
+        printfn("Количество не может быть отрицательным или равным нулю!")
+    0
---
+++ TASK2/TASK2/Program.fs
@@ -0,0 +1,20 @@
+﻿let rec minimal_digit number min = //рекурсивная функция для вычисления минимальной цифры числа
+    let  newmin = min
+    if number = 0 then 
+        newmin
+    else 
+    if min>(number%10) then
+         minimal_digit (number/10) (number%10)
+    else
+        minimal_digit (number/10) min
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите натуральное число: "
+    let num = int(System.Console.ReadLine())
+    if num > 0 then //проверка на корректность ввода
+        let min = minimal_digit num 9
+        printfn "Минимальная цифра числа %d: %d" num min
+    else
+        printfn "Число должно быть натуральным!"
+    0
